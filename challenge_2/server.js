70f774ac1ca16f8202d22d5e6892618a1d126437
phucciTr <miabam26@gmail.com>
2021-05-28 14:20:27 -0700

    Set up dependencies

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
 
 
+const app = express();
+const port = 3001;
+
+app.use(express.static(path.join(__dirname, './public')));
+app.use(cors());
+
+const server = app.listen(port, () => {
+  console.log(`listenting on port:${port}`);
+});
+
+
+
+module.exports = server;
