02fcfd65331a17aa597138c7880255a9afbbd444
phucciTr <miabam26@gmail.com>
2021-06-22 14:46:42 -0700

    Created PinsPad

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PinsPad from './components/PinsPad.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-
+      pins: 0,
+      frame: 1,
+      delivery: 1
     }
+
+    this.rollPins = this.rollPins.bind(this);
 
   }
 
   componentDidMount() {
 
+  }
+
+  rollPins(pins) {
+
+    let delivery = this.state.delivery;
+    let frame = this.state.frame;
+
+    this.setState({ pins: pins }, () => console.log('this.state.pins = ', this.state.pins));
+
+    if (delivery < 2) { return this.setState({ delivery: delivery + 1 }) }
+    this.setState({ delivery: 1, frame: frame + 1 });
   }
 
 
@@ -21,6 +37,10 @@ class App extends React.Component {
     return (
       <div>
         <h1>Bowling Game</h1>
+        {this.state.frame < 10 && <h2>{`Frame ${this.state.frame} Delivery ${this.state.delivery}`}</h2>}
+        {this.state.frame < 10 && <h4>Choose Number Of Pins Rolled</h4> }
+        {this.state.frame < 10 && <PinsPad handleClick={this.rollPins} /> }
+
       </div>
     );
   }
