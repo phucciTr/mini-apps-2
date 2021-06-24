17d3f05d36c6d26bfbe6e833dda06f5e11a2b993
phucciTr <miabam26@gmail.com>
2021-06-24 15:14:10 -0700

    Updated strike & spare visuals

--- challenge_3/client/components/Delivery.jsx
+++ challenge_3/client/components/Delivery.jsx
@@ -10,6 +10,19 @@ const Delivery = ({ frame, delivery, pins }) => {
 
   currentPins = currentPins === 0 ? 'pins' : currentPins;
 
+  currentPins = pins[frame] &&
+                delivery === 1 &&
+                pins[frame].first === 10
+                  ? 'X'
+                  : currentPins;
+
+  currentPins = pins[frame] &&
+                delivery === 1 &&
+                pins[frame].second > 0 &&
+                pins[frame].first + pins[frame].second === 10
+                  ? '/'
+                  : currentPins;
+
   return <td>{currentPins}</td>
 };
 
